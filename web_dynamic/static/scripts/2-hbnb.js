3590f3fb9f7d12d8034a81d89262cc97fb6f2aac
meghamohan <meghamohan1989@gmail.com>
2017-09-30 20:25:15 +0000

    adding  js file for task#3

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  let myDict = {};
+  $("input[type='checkbox']").click(function() {
+    if (this.checked) {
+      myDict[($(this).attr('data-id'))] = ($(this).attr('data-name'))
+    } else {
+      delete myDict[$(this).attr('data-id')];
+    }
+    let myString = Object.values(myDict)
+    let stringAmenities = myString.join(', ');
+    $('.amenities h4').text(stringAmenities);
+  })
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  })
+});
