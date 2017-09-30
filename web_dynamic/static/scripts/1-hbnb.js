73a761c46793911eca1af1696edae29839ab1adc
meghamohan <meghamohan1989@gmail.com>
2017-09-30 07:03:23 +0000

    adding tasks 0 & 1:styling yet to perfect

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  let myDict = {};
+  $("input[type='checkbox']").click(function() {
+    if (this.checked) {
+      myDict[($(this).attr('data-id'))] = ($(this).attr('data-name'))
+    } else {
+      delete myDict[$(this).attr('data-id')];
+    }
+    let myString = Object.values(myDict)
+    let stringAmenities = myString.join(', ');
+    $('.amenities h4').text(stringAmenities);
+  })
+});
