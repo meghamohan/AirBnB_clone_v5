888edcad04d6a4a2eef614f4483ddc20eebc3bbc
meghamohan <meghamohan1989@gmail.com>
2017-10-02 08:21:16 +0000

    adding task5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+  let myDict = {};
+  $("input[type='checkbox']").click(function() {
+    if (this.checked) {
+      myDict[($(this).attr('data-id'))] = ($(this).attr('data-name'))
+    } else {
+      delete myDict[$(this).attr('data-id')];
+    }
+    let myString = Object.values(myDict)
+    let stringAmenities = myString.join(', ');
+    $('.amenities h4').text(stringAmenities);
+  })
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  })
+  showPlaces({});
+ //task 4
+  function showPlaces (placesList) {
+    $.ajax({
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify(placesList),
+      error: function (response) {
+        $('.places').append('<p>Server issue</p>');
+        console.log(response);
+      },
+      success: function (response) {
+
+        let $places = $('.places');
+        $places.empty();
+        let $articles = $('<articles>');
+        $places.append($articles);
+
+        $(response).each( function(idx, place) {
+          //Max Guest
+          var $maxGuest = $('<div class="max_guest"></div>');
+          $maxGuest.append('<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />');
+          $maxGuest.append(place['max_guest'] + ' Guests');
+          //Num Rooms
+          var $numRooms = $('<div class="number_rooms"></div>');
+          $numRooms.append('<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />');
+          $numRooms.append(place['number_rooms'] + ' Rooms');
+          //Num Bathroom
+          var $numBathrooms = $('<div class="number_bathrooms">');
+          $numBathrooms.append('<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />');
+          $numBathrooms.append(place['number_bathrooms'] + ' Bathrooms');
+
+          $articles.append(
+            $('<div class="title"></div>').append(
+              $('<h2></h2>').text(place.name),
+              $('<div class="price_by_night"></div>').text('$' + place['price_by_night'])
+            ),
+            $('<div class="information">').append($maxGuest, $numRooms, $numBathrooms),
+            $('<div class="user"></div>'),
+            $('<div class="description"></div>').text(place['description'])
+          );
+        })
+      }
+    })
+  }
+  // task 5 adding search button ajax call
+  $('button').on('click', function () {
+    let checkBoxDict = {};
+    checkBoxDict['amenities'] = Object.keys(myDict);
+    showPlaces(checkBoxDict);
+  })
+
+});
